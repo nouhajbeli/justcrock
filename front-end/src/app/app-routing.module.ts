faebb16dac8dc45dc23b37fb8e96001b8a14deab
nouha jbeli <jbelinouha9@gmail.com>
2021-03-10 09:36:05 +0100

    front

---
+++ front-end/src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { TmplAstBoundAttribute } from '@angular/compiler';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import  {AboutComponent} from './components/about/about.component'
+const routes: Routes = [
+  {path:'about',component:AboutComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ front-end/src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { CardsComponent } from './components/cards/cards.component';
+import { AboutComponent } from './components/about/about.component';
+import { ToprecetteComponent } from './components/toprecette/toprecette.component';
+import { MembresComponent } from './components/membres/membres.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomepageComponent,
+    NavbarComponent,
+    CardsComponent,
+    AboutComponent,
+    ToprecetteComponent,
+    MembresComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MDBBootstrapModule.forRoot(),
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
